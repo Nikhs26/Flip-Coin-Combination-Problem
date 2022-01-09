12d16a53231331519f4c7181ab83772f07621772
Nikhil Gupta <nikhilgahoi1731997@gmail.com>
2022-01-09 10:12:00 +0530

    Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,29 +1,54 @@
-#! /bin/bash -x
+#! /bin/bash
+
+echo "Welcome to flip coin combination"
 
 declare -A flipCoin
-read -p "How many times you want to flip coin " noOfTime
-
 IS_HEAD=1
 counter=0
 headCount=0
 tailCount=0
 
+read -p "How many times you want to flip coin " noOfTime
+
+#read -p "How many coins you want to flip at same time " noOfCoin
+
+function flipCoinFunction(){
+
+	flipCoinResult=$((RANDOM%2))
+	#echo $flipCoinResult
+
+	if [ $flipCoinResult -eq $IS_HEAD ]
+	then
+		((headCount++))
+		echo "H"
+	else
+		echo "T"
+		((tailCount++))
+	fi
+}
+
 while [ $counter -lt $noOfTime ]
 do
-	flipCoinResult=$((RANDOM%2))
-	if [ $flipCoinResult -eq $IS_HEAD ]
-        then
-                echo "Head"
-		flipCoin[$counter]="Head"
-		((headCount++))
-        else
-                echo "Tail"
-		flipCoin[$counter]="Tail"
-		((tailCount++))
-        fi
-((counter++))
+		returnValueFromFunction=`flipCoinFunction`
+		returnValueFromFunction1=`flipCoinFunction`
+		doubletCombination="$returnValueFromFunction$returnValueFromFunction1"
+		flipCoin[$counter]=$doubletCombination
+		if [ ${flipCoin[$counter]} == 'HH' ]
+		then
+			((headCount++))
+		else
+			((tailCount++))
+		fi
+#		flipCoin[$counter]=`echo ${flipCoin[$counter]}$returnValueFromFunction`
+		((counter++))
 done
 
+for key in "${!flipCoin[@]}"; do
+    echo "$key ${flipCoin[$key]}"
+done
+
+
+echo $headCount $tailCount
 #Percentage of  head
 percnetageOfHead=$(((($headCount)*100)/$noOfTime))
 echo "Percentage of head " $percnetageOfHead
@@ -31,3 +56,4 @@ echo "Percentage of head " $percnetageOfHead
 #Percentage of tail
 percentageofTail=$((100-$percnetageOfHead))
 echo "Percentage of tail "$percentageofTail
+
